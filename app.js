0ac6a64ee9554c69b5837d430b3e5dd7378969a7
Darkcodelab <deepanchakravarthi434@gmail.com>
2022-04-27 20:27:59 +0530

    Updated styles and functions

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 (function () {
-    let boxes = document.getElementsByClassName("box");
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('mouseover', () => boxes[i].childNodes[1].classList.add('bg-zoom'))
-        boxes[i].addEventListener('mouseout', () => boxes[i].childNodes[1].classList.remove('bg-zoom'))
-    }
+  let boxes = document.getElementsByClassName("box");
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("mouseover", () => {
+      boxes[i].children[0].classList.add("bg-zoom");
+    });
+    boxes[i].addEventListener("mouseout", () =>
+      boxes[i].children[0].classList.remove("bg-zoom")
+    );
+  }
 })();
+
 let buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        let soundElem = document.getElementById(button.value)
-        if (button.innerText == 'play') {
-            button.innerText = 'stop'
-            playMusic(soundElem)
-        } else {
-            button.innerText = 'play'
-            stopMusic(soundElem)
-        }
-    })
-})
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let soundElem = document.getElementById(button.value);
+    if (button.innerText == "play") {
+      button.innerText = "stop";
+      playMusic(soundElem);
+    } else {
+      button.innerText = "play";
+      stopMusic(soundElem);
+    }
+  });
+});
 
 function playMusic(name) {
-    name.play();
-    name.vol = 0.5;
+  name.play();
+  name.volume = 0.5;
 }
 
 function stopMusic(name) {
-    name.pause();
-    name.currentTime = 0;
+  name.pause();
+  name.currentTime = 0;
 }
-let slider = document.getElementsByClassName('slider');
+let slider = document.getElementsByClassName("slider");
 for (let i = 0; i < slider.length; i++) {
-    slider[i].addEventListener("input", function () {
-        let vol = slider[i].value / 100
-        let audio = slider[i].previousElementSibling;
-        audio.volume = vol
-    })
+  slider[i].addEventListener("input", function () {
+    let vol = slider[i].value / 100;
+    let audio = slider[i].previousElementSibling;
+    audio.volume = vol;
+  });
 }
